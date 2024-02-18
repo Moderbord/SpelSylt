084004fece95e134b7d470ef22d4e8ef683ad6d8
Fredrik Lind <fredrik.lind.96@gmail.com>
2024-02-18 15:40:40 +0100

    Add source, submodules, remove FMODStudio local

---
+++ Source/SpelSylt/Public/Recipe/GoblinIngredient.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GoblinIngredient.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPELSYLT_API UGoblinIngredient : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
